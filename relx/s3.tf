66ef7b45b1a21e94f36675982419e4cf41c4187c
deyrahul29 <75727817+deyrahul29@users.noreply.github.com>
2022-11-14 12:12:29 +0530

    add the policy

--- relx/s3.tf
+++ relx/s3.tf
@@ -5,6 +5,23 @@ resource "aws_s3_bucket" "relxAutomation" {
   # versioning {
   #  enabled = true
   #}
+  
+    policy = <<EOF
+{
+  "Version": "2008-10-17",
+  "Statement": [
+    {
+      "Sid": "PublicReadForGetBucketObjects",
+      "Effect": "Allow",
+      "Principal": {
+        "AWS": "*"
+      },
+      "Action": "s3:GetObject",
+      "Resource": "arn:aws:s3:::${var.bucketname}/*"
+    }
+  ]
+}
+EOF
 
   website {
     index_document = "index.html"
